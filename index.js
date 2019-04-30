caf50452622e92fd0976549aee385da23bb95ce1
Jason Leach <jlspursfan@gmail.com>
2019-04-30 20:35:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,14 +11,14 @@ function logShout(string) {
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase)
+  console.log(string.toLowerCase())
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === lowercase) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-  else if (string.toUpperCase() === uppercase) {
+  else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
   else if (string === "I love you, Grandma.") {
